1f20facdc2e505609664060de9536be72e902089
Lucas Lopilato <lopilato.lucas@gmail.com>
2016-11-28 14:15:27 -0800

    Added Added CircleOfFriends CRUD Class

---
+++ src/BuzMo/Database/CircleOfFriends.java
@@ -0,0 +1,75 @@
+package BuzMo.Database;
+
+import BuzMo.Logger.Logger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+/**
+ * Created by lucas on 11/28/2016.
+ * CRUD Class for CircleOfFriends
+ */
+public class CircleOfFriends extends DatabaseObject {
+    public CircleOfFriends(Logger log, Connection connection) throws DatabaseException {
+        super(log, connection);
+    }
+
+    //Get the circle of Friends for a user
+    //Note: Does not contain the user whose circle it is
+    public Vector<String> getCircleOfFriends(String userID)throws DatabaseException{
+        Vector<String> response = new Vector<>();
+
+        //Check if the user exists
+        if(!User.exists(st,userID)){
+            throw new DatabaseException("Cannot find circle of Friends for non existent user: "+userID);
+        }
+        String sql = "SELECT * FROM circleOfFriends C WHERE C.user="+addTicks(userID)+" OR C.friend="+addTicks(userID);
+        try {
+            st.execute(sql);
+            ResultSet rs = st.getResultSet();
+
+            while(rs.next()){
+                String first = rs.getString("user");
+                if(first.compareTo(userID) != 0){
+                    response.add(first);
+                }
+                else{
+                    response.add(rs.getString("friend"));
+                }
+            }
+
+            log.Log("Circle of Friends Retrieved for user "+userID);
+            return response;
+        } catch (SQLException e) {
+            log.Log("Invalid SQL: "+sql);
+            throw new DatabaseException(e);
+        }
+    }
+
+
+    //Add new Friendship
+    public void addFriends(String user1, String user2) throws DatabaseException {
+        //Check if both users are the same
+        if(user1.compareTo(user2) == 0){
+            throw new DatabaseException("Users are implicitly friends with each other");
+        }
+
+        if(!User.exists(st, user1)){
+            throw new DatabaseException("Cannot add friendship, "+user1+" doesn't exist");
+        }
+
+        if(!User.exists(st, user2)){
+            throw new DatabaseException("Cannot add friendship, "+user2+" doesn't exist");
+        }
+
+        String sql = "INSERT INTO circleoffriends (user,friend) VALUES (" +addTicks(user1)+","+addTicks(user2)+")";
+        try {
+            st.execute(sql);
+        } catch (SQLException e) {
+            log.Log("Invalid SQL: "+sql);
+            throw new DatabaseException(e);
+        }
+    }
+}
