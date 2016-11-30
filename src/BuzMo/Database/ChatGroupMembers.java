6222a76dc602fd7d11055c812fb9371df45d1694
Lucas Lopilato <lopilato.lucas@gmail.com>
2016-11-29 18:17:33 -0800

    Added ChatGroupMembers CRUD Class

---
+++ src/BuzMo/Database/ChatGroupMembers.java
@@ -0,0 +1,72 @@
+package BuzMo.Database;
+
+import BuzMo.Logger.Logger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+/**
+ * Created by lucas on 11/29/2016.
+ * CRUD Class for ChatGroupMembers Table
+ */
+public class ChatGroupMembers extends DatabaseObject {
+
+    public ChatGroupMembers(Logger log, Connection connection) throws DatabaseException {
+        super(log, connection);
+    }
+
+    //Gets a list of all recipients for a specific message
+    public Vector<String> members(String groupName) throws DatabaseException{
+        Vector<String> response = new Vector<>();
+
+        //Check if the user exists
+        if(!ChatGroups.exists(st,groupName)){
+            throw new DatabaseException("Cannot find recipients for non existent group: "+groupName);
+        }
+        String sql = "SELECT member FROM chatgroupmembers C WHERE C.group_name="+addTicks(groupName);
+        try {
+            st.execute(sql);
+            ResultSet rs = st.getResultSet();
+
+            while(rs.next()){
+                response.add(rs.getString("member"));
+            }
+
+            log.Log("Members retrieved for "+groupName);
+            return response;
+        } catch (SQLException e) {
+            log.Log("Invalid SQL: "+sql);
+            throw new DatabaseException(e);
+        }
+    }
+
+
+    //Attempt to insert members for a chat group
+    public Insert insertMembers(String groupName, Vector<String> members) throws DatabaseException{
+        String sql = "INSERT INTO chatgroupmembers (group_name, member) VALUES (";
+
+        if(!ChatGroups.exists(st, groupName)){
+            return Insert.NOEXIST_GROUP;
+        }
+
+        //Check if the message exists
+        //Associate each user with a groupName
+        for(String s: members){
+            if(!User.exists(st, s)){
+                return Insert.NOEXIST_USR;
+            }
+
+            sql += addTicks(groupName) + "," + addTicks(s) + ')';
+
+            try {
+                st.execute(sql);
+            } catch (SQLException e) {
+                throw new DatabaseException(e);
+            }
+        }
+
+        return Insert.SUCCESS;
+    }
+}
