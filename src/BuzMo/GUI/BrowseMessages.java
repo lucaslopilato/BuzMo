9e3f26bc714718d73b22b8372407fef1be9f54da
Ben Lei <benjamin.g.lei@gmail.com>
2016-11-30 23:16:09 -0800

    Updated GUI -- Added BrowseMessages option (WIP)

--- src/BuzMo/GUI/BrowseMessages.java
+++ src/BuzMo/GUI/BrowseMessages.java
@@ -1,7 +1,74 @@
 package BuzMo.GUI;
 
+import BuzMo.Logger.Logger;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 /**
  * Created by Ben on 11/30/2016.
  */
 public class BrowseMessages {
+    private Logger log;
+    private JFrame browseMessagesWindow = new JFrame("Search for Messages or Users");
+    private JButton messageSearchButton =  new JButton("Search Messages");
+    private JButton userSearchButton =  new JButton("Search Users");
+    private JTextField messageSearchField = new JTextField(20);
+    private JTextField userSearchField = new JTextField(20);
+
+    public BrowseMessages(Logger log, String yourUsername) {
+        // Hook up logger to GUI
+        this.log = log;
+
+        // Set the login panel parameters
+        browseMessagesWindow.setSize(300, 200);
+        browseMessagesWindow.setLocation(100, 200);
+        browseMessagesWindow.setResizable(false);
+
+        // Create the main panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null);
+
+        // Create labels
+        JLabel messageSearchText = new JLabel("Search Messages:");
+        JLabel userSearchText = new JLabel("Search Users:");
+
+        // Set the field + button parameters
+        messageSearchText.setBounds(20, 20, 150, 20);
+        messageSearchField.setBounds(150, 20, 100, 20);
+        messageSearchButton.setBounds(100, 50, 150, 20);
+
+        userSearchText.setBounds(20,95,150,20);
+        userSearchField.setBounds(150, 95, 100, 20);
+        userSearchButton.setBounds(100, 125, 150, 20);
+
+        // Add the fields + buttons to the login panel
+        mainPanel.add(messageSearchField);
+        mainPanel.add(userSearchField);
+        mainPanel.add(messageSearchText);
+        mainPanel.add(userSearchText);
+        mainPanel.add(messageSearchButton);
+        mainPanel.add(userSearchButton);
+
+        // Add the login panel + ActionListeners to the Content Pane
+        browseMessagesWindow.setContentPane(mainPanel);
+        browseMessagesWindow.setVisible(true);
+
+        handleSearchButtonClick(log, yourUsername, messageSearchField.getText());
+        handleSearchButtonClick(log, yourUsername, userSearchField.getText());
+
+        log.Log("GUI -- BrowseMessages properly loaded");
+    }
+
+    private void handleSearchButtonClick(Logger log, String yourUsername, String input) {
+        messageSearchButton.addActionListener(
+                (ActionEvent e) -> {
+                    new TopicSearchResults(log, yourUsername, input);
+                }
+        );
+        userSearchButton.addActionListener(
+                (ActionEvent e) -> {
+                    new UserSearchResults(log, yourUsername, input);
+                }
+        );
+    }
 }
---
+++ src/BuzMo/GUI/UserSearchResults.java
@@ -0,0 +1,15 @@
+package BuzMo.GUI;
+
+import BuzMo.Logger.Logger;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+/**
+ * Created by Ben on 11/30/2016.
+ * Return a list of users matching the input.
+ * OPTIONAL(?): send them a friend request
+ */
+public class UserSearchResults {
+    UserSearchResults(Logger log, String yourUsername, String input) {
+        JOptionPane.showMessageDialog(null, "Created UserSearchResults");
+    }
+}
